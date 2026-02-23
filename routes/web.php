39ff510363755bd0b4898dea34d73d18fa970c47
404Gh0st <60356707+404GH0ST@users.noreply.github.com>
2026-02-23 09:57:46 +0700

    feat: tambah route P1

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,29 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang';
+});
+
+Route::get("/hello", function () {
+    return 'Hello World';
+});
+
+Route::get("/world", function () {
+    return 'World';
+});
+
+Route::get('/about', function () {
+    return '244107020049 - Agus Prasetyo';
+});
+
+Route::get("/user/{name?}", function ($name='John') {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." dan Komentar ke-: ".$commentId;
+});
+
+Route::get("/articles/{id}", function ($articleId) {
+    return 'Halaman Artikel dengan ID '.$articleId;
 });
